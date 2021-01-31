fb70382dfce530bb3e30ac5e4061108a033cc35e
Olayinka <holahyeankah12@gmail.com>
2021-01-31 07:28:30 +0100

    commit changes

---
+++ build/precache-manifest.25d40c337ed90469745bcb76bca62607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa18bcc3a0c4d13a187af8a7d0f4346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9c2046f69eca166b417",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ee34f4b1d726449090c0",
+    "url": "/static/css/main.5015585b.chunk.css"
+  },
+  {
+    "revision": "b9c2046f69eca166b417",
+    "url": "/static/js/2.7ddbcaf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ddbcaf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee34f4b1d726449090c0",
+    "url": "/static/js/main.50274bc2.chunk.js"
+  },
+  {
+    "revision": "3a3b720ac7166cde82fa",
+    "url": "/static/js/runtime-main.ff1f797a.js"
+  }
+]);
